2712f7d32114247b3ec06be25abf2609d5789b2f
Erickkipkoech <cheruiyotkipkoech612@gmail.com>
2025-01-19 19:17:43 +0300

    dist file commit

--- dist/angular-dashboard/server/angular-app-manifest.mjs
+++ dist/angular-dashboard/server/angular-app-manifest.mjs
@@ -11,9 +11,9 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 25195, hash: 'c95605b3e2054efdf8097b721eca3e0d37683c95aa8cadc93273852e8f65662b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17195, hash: 'fe93cc3ba1e49260b261cc2e8c13b21da8de46e8df83b54f92024d45fc602296', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 51793, hash: '7701ad09d4ce6816df84cd6fc6303cefd7b0ff5a2a7c0c7a0c3b7b0f684146c3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-X5UJMOIL.css': {size: 19605, hash: 'lJShwQNfrP4', text: () => import('./assets-chunks/styles-X5UJMOIL_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 25196, hash: 'e16ffc324f340fbff6960de3cc329e880dc68da5448efaa998252935281176af', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17196, hash: 'fff06dc326d6cec6a6a8a1f5a7d0c8335f50eab2448d9c0e475ace9e238a2911', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 52935, hash: 'e237efdf70266530163cd282a45198ce838d76e69256e2ab078559df5b272cdd', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-NORZC6OF.css': {size: 20611, hash: 'DVkIfEesptY', text: () => import('./assets-chunks/styles-NORZC6OF_css.mjs').then(m => m.default)}
   },
 };
